bee77bdfe446972ef54c05a5d03ccfc8da558faa
ytraddan <1nightshelter@gmail.com>
2025-01-05 17:31:45 +0300

    fix padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
-    <main className=" w-full p-8 px-12 overflow-y-scroll">
+    <main className=" w-full p-12 md:py-20 overflow-y-scroll">
       <div className="content max-w-5xl mx-auto space-y-24">
         <Hero />
         <Skills />
